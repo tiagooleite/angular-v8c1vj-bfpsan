ad9566cb2d62794aa7f85d37efeaa5b858e9356f
tiagooleite <tiagooleite@gmail.com>
2019-12-11 19:09:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-v8c1vj-bfpsan
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-v8c1vj-bfpsan)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Aquathlon do Sol',
+    price: 799,
+    description: 'O aquatlon que promete agitar o verão de Pirangi está de volta na sua 4° Edição. Com percurso de 1.000mts de natação + 5k de corrida, você poderá participar em solo ou dupla de revezamento. Aproveite o lote promocional até o dia 23/12 e venha viver essa experiência surreal na badalada praia de Pirangi do Norte!'
+  },
+  {
+    name: 'Pirangi Praiano',
+    price: 699,
+    description: 'Corrida na praia de Pirangi'
+  },
+  {
+    name: '10km corrida maluka',
+    price: 299,
+    description: 'Testando integração com git'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
